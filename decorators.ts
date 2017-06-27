b440b3bc366cc67203f5285f477a1a3ead999cf6
Shruthi <ShruthiShankar@users.noreply.github.com>
2017-06-27 21:33:42 +0530

    4th assignment of 4th session

---
+++ decorators.ts
@@ -0,0 +1,13 @@
+function setSkills(target){
+	target.prototype.skills = "cricket";
+}
+
+/*@setSkills is the class decorator, decorators is denoted with @ symbol*/
+@setSkills
+class player{
+	skills : String;
+}
+
+var sachin = new player();
+console.log(sachin.skills);
+
